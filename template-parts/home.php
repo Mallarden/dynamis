a30515cd4708f6d481db676b4030e39a374cc6d0
Mallarden <antonanderson.com@gmail.com>
2019-12-10 12:13:34 +0100

    fixes

--- template-parts/home.php
+++ template-parts/home.php
@@ -60,8 +60,8 @@ get_header(); ?>
               <div class="d-flex pt-2" style="flex-flow: wrap;">
                 <?php if( have_rows('direct_impact') ):
 							while ( have_rows('direct_impact') ) : the_row(); ?>
-                <div class="col col-lg-4 fade-y pr-0" style="" data-delighter="start:0.92;">
-                  <img class="" src="<? the_sub_field('direct_impact_bild');?>" />
+                <div class="col col-lg-6 fade-y pr-0" style="" data-delighter="start:0.92;">
+                  <img class="mt-3 d-flex" style="margin: 0 auto;" src="<? the_sub_field('direct_impact_bild');?>" />
                 </div>
                 <?php endwhile;	else :endif; ?>
               </div>
@@ -91,7 +91,7 @@ get_header(); ?>
       <div class="row" data-delighter="start:0.90;">
         <div class="col-sm-12 col-lg-6 fade-y">
           <div class="our-team-img-info">
-            <div class="col-xs-12 col-sm-6 fade-y personbild"
+            <div class="col-xs-12 col-sm-6 fade-y personbild pt-4"
               style="background-image:url('<?php the_sub_field('personbild'); ?>')"></div>
             <div class="col-xs-12 col-sm-6 fade-y pt-4" style="max-width: max-content;
     margin: 0 auto;">
@@ -106,7 +106,7 @@ get_header(); ?>
       </div>
       <div class="container mt-5">
         <div class="row">
-          <div class="col-lg-6 col-sm-12">
+          <div class="col-lg-6 col-sm-12" style="justify-content: center;align-items: center; display: flex;">
             <img class="teambild pt-3" src="<? the_sub_field('teambild');?>" />
           </div>
           <div class="col-lg-6 col-sm-12 mt-4">
