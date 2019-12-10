9326770f0b6ef714223572e85a4cd4b632b52637
Mallarden <antonanderson.com@gmail.com>
2019-12-10 16:13:25 +0100

    latest fixes

--- template-parts/home.php
+++ template-parts/home.php
@@ -91,7 +91,7 @@ get_header(); ?>
       <div class="row" data-delighter="start:0.90;">
         <div class="col-sm-12 col-lg-6 fade-y">
           <div class="our-team-img-info">
-            <div class="col-xs-12 col-sm-6 fade-y personbild pt-4"
+            <div class="col-xs-12 col-sm-6 fade-y personbild pt-5"
               style="background-image:url('<?php the_sub_field('personbild'); ?>')"></div>
             <div class="col-xs-12 col-sm-6 fade-y pt-4" style="max-width: max-content;
     margin: 0 auto;">
